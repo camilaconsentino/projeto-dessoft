503f63919229a6352345c4bc5af62f5238014d16
maria vitoria <mariavjs@al.insper.edu.br>
2022-05-11 21:20:48 -0300

    cores inclusas

--- stage.py
+++ stage.py
@@ -4,6 +4,10 @@ from math import *
 import random
 from funcoes import normaliza, haversine, sorteia_pais, esta_na_lista, sorteia_letra, adiciona_em_ordem, sorteia_cor
 from dicionario import hacker
+import colorama 
+from colorama import Fore
+colorama.init(autoreset=True)
+
 
 #definindo variaveis
 a = ''
@@ -77,28 +81,73 @@ print('inventario ------ mostra sua posicao\n')
 
 
 continuar = True
-comando = input('\nChute um país!{0} ' .format('\U0001F929'))
+comando = input(Fore.WHITE + '\nChute um país!{0} ' .format('\U0001F929'))
 
 
 
 while continuar:
 
     if tentativas == 1:
-        print('\nAcabaram suas tentativas! O país correto era {0}\n' .format(pais_sorteado))
+        print(Fore.WHITE +'\nAcabaram suas tentativas! O país correto era {0}\n' .format(pais_sorteado))
         break
 
     if comando == 'dica':
-        print('\nMERCADO DE DICAS')        
-        print('------------------------------------------')
-        print('1. Cor da bandeira --> custa 4 tentativas')
-        print('2. Letra da capital -> custa 3 tentativas')
-        print('3. Área -------------> custa 6 tentativas')
-        print('4. População --------> custa 5 tentativas')
-        print('5. Continente -------> custa 7 tentativas')
-        print('0. Sem dicas --------> {0}               ' .format('\U0001F910'))
-        print('------------------------------------------')
+        if tentativas < 3:
+            print('\nMERCADO DE DICAS')        
+            print('------------------------------------------')
+            print(Fore.RED +'1. Cor da bandeira --> custa 4 tentativas')
+            print(Fore.RED +'2. Letra da capital -> custa 3 tentativas')
+            print(Fore.RED +'3. Área -------------> custa 6 tentativas')
+            print(Fore.RED +'4. População --------> custa 5 tentativas')
+            print(Fore.RED +'5. Continente -------> custa 7 tentativas')
+            print(Fore.WHITE +'0. Sem dicas --------> {0}               ' .format('\U0001F910'))
+            print('------------------------------------------')
 
-        dica = input('\nEscolha sua dica [0|1|2|3|4|5]: ')
+        elif tentativas == 4:
+            print('\nMERCADO DE DICAS')        
+            print('------------------------------------------')
+            print(Fore.GREEN +'1. Cor da bandeira --> custa 4 tentativas')
+            print(Fore.GREEN +'2. Letra da capital -> custa 3 tentativas')
+            print(Fore.RED +'3. Área -------------> custa 6 tentativas')
+            print(Fore.RED +'4. População --------> custa 5 tentativas')
+            print(Fore.RED +'5. Continente -------> custa 7 tentativas')
+            print(Fore.WHITE +'0. Sem dicas --------> {0}               ' .format('\U0001F910'))
+            print('------------------------------------------')
+
+        elif tentativas == 5:
+            print('\nMERCADO DE DICAS')        
+            print('------------------------------------------')
+            print(Fore.GREEN +'1. Cor da bandeira --> custa 4 tentativas')
+            print(Fore.GREEN +'2. Letra da capital -> custa 3 tentativas')
+            print(Fore.RED +'3. Área -------------> custa 6 tentativas')
+            print(Fore.GREEN +'4. População --------> custa 5 tentativas')
+            print(Fore.RED +'5. Continente -------> custa 7 tentativas')
+            print(Fore.WHITE +'0. Sem dicas --------> {0}               ' .format('\U0001F910'))
+            print('------------------------------------------')
+
+        elif tentativas == 6:
+            print('\nMERCADO DE DICAS')        
+            print('------------------------------------------')
+            print(Fore.GREEN +'1. Cor da bandeira --> custa 4 tentativas')
+            print(Fore.GREEN +'2. Letra da capital -> custa 3 tentativas')
+            print(Fore.GREEN +'3. Área -------------> custa 6 tentativas')
+            print(Fore.GREEN +'4. População --------> custa 5 tentativas')
+            print(Fore.RED +'5. Continente -------> custa 7 tentativas')
+            print(Fore.WHITE +'0. Sem dicas --------> {0}               ' .format('\U0001F910'))
+            print('------------------------------------------')
+
+        elif tentativas > 7:
+            print('\nMERCADO DE DICAS')        
+            print('------------------------------------------')
+            print(Fore.GREEN +'1. Cor da bandeira --> custa 4 tentativas')
+            print(Fore.GREEN +'2. Letra da capital -> custa 3 tentativas')
+            print(Fore.GREEN +'3. Área -------------> custa 6 tentativas')
+            print(Fore.GREEN +'4. População --------> custa 5 tentativas')
+            print(Fore.GREEN +'5. Continente -------> custa 7 tentativas')
+            print(Fore.WHITE +'0. Sem dicas --------> {0}               ' .format('\U0001F910'))
+            print('------------------------------------------')
+
+        dica = input(Fore.WHITE + '\nEscolha sua dica [0|1|2|3|4|5]: ')
 
         #cor da bandeira
         if dica == '1':
@@ -108,7 +157,7 @@ while continuar:
                 print('Dica: {0}' .format(verifica_cores))
 
             else: #tentar ver o bang das cores
-                print('\nVocê não tem saldo!{0}' .format('\U0001F643'))
+                print(Fore.WHITE + '\nVocê não tem saldo!{0}' .format('\U0001F643'))
         
         #letra da capital
         elif dica == '2':
@@ -117,10 +166,10 @@ while continuar:
                 capital = capitais[pais_sorteado]
                 letra = sorteia_letra(capital, letras_sorteadas)
                 letras_sorteadas.append(letra)
-                print('\nLetra da capital: {0}' .format(letras_sorteadas))
+                print(Fore.WHITE + '\nLetra da capital: {0}' .format(letras_sorteadas))
 
             else: #tentar ver o bang das cores
-                print('\nVocê não tem saldo!{0}' .format('\U0001F643'))
+                print(Fore.WHITE + '\nVocê não tem saldo!{0}' .format('\U0001F643'))
         
         #area
         elif dica == '3':
@@ -130,10 +179,10 @@ while continuar:
                     tentativas -= 6
                     dicas_repetidas.append(dica)
 
-                print('\nÁrea do país é: {0}km2'.format(area))
+                print(Fore.WHITE + '\nÁrea do país é: {0}km2'.format(area))
 
             else: #tentar ver o bang das cores
-                print('\nVocê não tem saldo!{0}' .format('\U0001F643'))
+                print(Fore.RED + '\nVocê não tem saldo!{0}' .format('\U0001F643'))
 
         #populacao
         elif dica == '4':
@@ -143,10 +192,10 @@ while continuar:
                     tentativas -= 5
                     dicas_repetidas.append(dica)
                 
-                print('\nA população do país é: {0} habitantes' .format(populacao))
+                print(Fore.WHITE + '\nA população do país é: {0} habitantes' .format(populacao))
             
             else: #tentar ver o bang das cores
-                print('\nVocê não tem saldo!{0}' .format('\U0001F643'))
+                print(Fore.WHITE + '\nVocê não tem saldo!{0}' .format('\U0001F643'))
 
         #continente
         elif dica == '5':
@@ -156,21 +205,21 @@ while continuar:
                     tentativas -= 7
                     dicas_repetidas.append(dica)
                 
-                print('\nO continente do país é: {0}'.format(continente))
+                print(Fore.WHITE + '\nO continente do país é: {0}'.format(continente))
             
             else: #tentar ver o bang das cores
-                print('\nVocê não tem saldo!{0}' .format('\U0001F643'))
+                print(Fore.RED +'\nVocê não tem saldo!{0}' .format('\U0001F643'))
 
     elif comando == 'inventario':
-        print('\nVocê ainda tem {0} tentativas!{1}' .format(tentativas, '\U0001F618')) 
+        print(Fore.WHITE + '\nVocê ainda tem {0} tentativas!{1}' .format(tentativas, '\U0001F618')) 
 
     elif comando == 'desistir':
-        print('\nAté mais!\n')
+        print(Fore.WHITE + '\nAté mais!\n')
         break
 
     elif comando == pais_sorteado:
-        print('Você acertou!{0}' .format('\U0001F606'))
-        repete = input('\nJogar novamente [s|n]?{0}' .format('\U0001F60D'))
+        print(Fore.WHITE + 'Você acertou!{0}' .format('\U0001F606'))
+        repete = input(Fore.WHITE + '\nJogar novamente [s|n]?{0}' .format('\U0001F60D'))
 
         if repete == 's':
             continuar
@@ -179,20 +228,26 @@ while continuar:
             continuar = False
     
     elif comando not in paises:
-        print('\nEsse pais não existe{0}' .format('\U0001F928'))    
+        print(Fore.WHITE + '\nEsse pais não existe{0}' .format('\U0001F928'))    
 
     else:
         distancia = haversine(raio, latitudes[pais_sorteado], longitudes[pais_sorteado], latitudes[comando], longitudes[comando])
         palpite = [comando, distancia]
         estanalista = esta_na_lista(palpite, palpites_anteriores)
         if estanalista == True:
-            print('\nVocê já tentou esse país!{0}' .format('\U0001F644'))
+            print(Fore.WHITE + '\nVocê já tentou esse país!{0}' .format('\U0001F644'))
         else:
             palpites_anteriores = adiciona_em_ordem(comando, distancia, palpites_anteriores)
             tentativas -= 1
             for palpite in palpites_anteriores:
                 dis = palpite[1]
                 nome = palpite[0]
-                print('\nPaís: {0} -> Distância: {1:,.0f}km' .format(nome, dis))
+                if dis <= 4000:
+                    print(Fore.GREEN + '\nPaís: {0} -> Distância: {1:,.0f}km' .format(nome, dis))
+                elif dis <= 9000:
+                    print(Fore.YELLOW + '\nPaís: {0} -> Distância: {1:,.0f}km' .format(nome, dis))
+                else:
+                    print(Fore.RED + '\nPaís: {0} -> Distância: {1:,.0f}km' .format(nome, dis))
+                
 
-    comando = input('\nChute um país!{0} ' .format('\U0001F929'))
+    comando = input(Fore.WHITE + '\nChute um país!{0} ' .format('\U0001F929'))
